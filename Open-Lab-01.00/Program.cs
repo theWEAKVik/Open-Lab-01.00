8c6089d844081eebb91820e2e000c9bfbe0bffa1
theWEAKVik <71783156+theWEAKVik@users.noreply.github.com>
2020-09-29 17:01:28 +0200

    Update Program.cs

--- Open-Lab-01.00/Program.cs
+++ Open-Lab-01.00/Program.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_01._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Apps Lab!");
         }
     }
 }
